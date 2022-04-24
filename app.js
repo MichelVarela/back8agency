3eb5303b88f79eecbbd9208313974b3a0bf4a57f
MichelVarela <michelvarela.95@outlook.com>
2022-04-24 12:59:03 -0300

    updatev6

--- app.js
+++ app.js
@@ -45,9 +45,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
-
 module.exports = app;
